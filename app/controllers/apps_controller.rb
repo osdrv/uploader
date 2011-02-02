b65723adbdc030dcaa4b1ec05357a0af6902f3fb
4pcbr <i4pcbr@gmail.com>
2011-02-02 10:17:46 +0300

    Upload hostname generates while building user app

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -4,9 +4,12 @@ class AppsController < ApplicationController
 
   def create
     username = params[:username]
+    hostname = request.host_with_port
     system "echo \"#{username}\" > client/auc_bundle.app/Contents/Resources/username.name"
+    system "echo \"#{hostname}\" > client/auc_bundle.app/Contents/Resources/hostname.name"
     system "tar -cf #{username}_app.tar client/auc_bundle.app/"
     system "rm client/auc_bundle.app/Contents/Resources/username.name"
+    system "rm client/auc_bundle.app/Contents/Resources/hostname.name"
     send_file "#{username}_app.tar", :filename => "bundle.tar"
   end
 end
