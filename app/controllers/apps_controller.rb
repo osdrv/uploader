9c24bf7d42495170553d26889c55d4b92d758c46
4pcbr <o.s.sidorov@gmail.com>
2011-03-05 19:39:05 +0300

    думаю, это пофиксит проблему с битыми архивами

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -8,7 +8,7 @@ class AppsController < ApplicationController
     Dir.chdir(Rails.root.to_s) do
       system "echo \"#{username}\" > client/auc_bundle.app/Contents/Resources/username.name"
       system "echo \"#{hostname}\" > client/auc_bundle.app/Contents/Resources/hostname.name"
-      system "tar -cf #{username}_app.tar client/auc_bundle.app/"
+      system "tar -cvzpf #{username}_app.tar.gz client/auc_bundle.app/"
       system "rm client/auc_bundle.app/Contents/Resources/username.name"
       system "rm client/auc_bundle.app/Contents/Resources/hostname.name"
       send_file "#{Rails.root.to_s}/#{username}_app.tar", :filename => "bundle.tar"
