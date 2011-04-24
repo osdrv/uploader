ea434bf04baaf86e0702d78976d7fe30c96a9d9f
4pcbr <o.s.sidorov@gmail.com>
2011-04-24 22:34:08 +0400

    изменил название приложения

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -6,11 +6,11 @@ class AppsController < ApplicationController
     username = params[:username]
     hostname = request.host_with_port
     Dir.chdir(Rails.root.to_s) do
-      system "echo \"#{username}\" > client/auc_bundle.app/Contents/Resources/username.name"
-      system "echo \"#{hostname}\" > client/auc_bundle.app/Contents/Resources/hostname.name"
-      system "tar -cf #{username}_app.tar client/auc_bundle.app/"
-      system "rm client/auc_bundle.app/Contents/Resources/username.name"
-      system "rm client/auc_bundle.app/Contents/Resources/hostname.name"
+      system "echo \"#{username}\" > client/uploader.app/Contents/Resources/username.name"
+      system "echo \"#{hostname}\" > client/uploader.app/Contents/Resources/hostname.name"
+      system "tar -cf #{username}_app.tar client/uploader.app/"
+      system "rm client/uploader.app/Contents/Resources/username.name"
+      system "rm client/uploader.app/Contents/Resources/hostname.name"
       send_file "#{Rails.root.to_s}/#{username}_app.tar", :filename => "bundle.tar", :type => 'application/x-tar'
     end
   end
